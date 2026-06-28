89466e65bb764b146a41df7d3e4880d7ebb407c2
NandanKumarjv <nandanjv04@gmail.com>
2026-06-28 12:25:44 +0530

    Methods Programs are added

---
+++ Methods/Methodwithargu.java
@@ -0,0 +1,13 @@
+package class5;
+
+public class Methodwithargu {
+    void greet(int a) {
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        Methodwithargu obj = new Methodwithargu();
+        obj.greet(10);
+    }
+}
+
---
+++ Methods/MethodwoArgu.java
@@ -0,0 +1,12 @@
+package class5;
+
+public class MethodwoArgu {
+    void greet() {
+        System.out.println("Welcome to java");
+    }
+
+    public static void main(String[] args) {
+        MethodwoArgu obj = new MethodwoArgu();
+        obj.greet();
+    }
+}
