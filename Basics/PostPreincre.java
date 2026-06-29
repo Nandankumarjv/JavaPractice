1677a29ee829d11120b9136ed200004032dd95d4
NandanKumarjv <nandanjv04@gmail.com>
2026-06-29 19:11:12 +0530

    pre,post increment ,standard input programs added

---
+++ Basics/PostPreincre.java
@@ -0,0 +1,12 @@
+package class4;
+
+public class PostPreincre {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        boolean result = (a < b) && (b > 15);
+
+        System.out.println(result);
+    }
+}
---
+++ Basics/Stdin.java
@@ -0,0 +1,18 @@
+package class5;
+import java.util.Scanner;
+public class Stdin {
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter the name");
+        String name=scn.nextLine();
+        System.out.println("Enter the usn");
+        int usn=scn.nextInt();
+        System.out.println("Enter the phone number");
+        long phno=scn.nextLong();
+        System.out.println("enter the fees");
+        double fees=scn.nextDouble();
+        System.out.println("enter the gender");
+        Boolean gender=scn.nextBoolean();
+        
+    }
+}
